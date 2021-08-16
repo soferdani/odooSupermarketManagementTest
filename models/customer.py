5612184fa698a791fadb59c508795f6a4a63930a
Dani Sofer <soferdani@gmail.com>
2021-08-16 17:43:41 +0300

    Update customer.py

--- models/customer.py
+++ models/customer.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class Customer(models.Model):
     _name = 'supermarket.customer'
     _description = 'Customer'
 
+    
+    
     name = fields.Char('Name', required=True)
     email = fields.Char('Email')
     city = fields.Char('City')
     street = fields.Char('Street')
+    
+    bestSellerItems = fields.Int(compute='_compute_bs')
+    
+    
+    def _compute_bs(self):
+        for record in self:
+            bestSellerSearch = self.env['supermarket.cartItem'].search([('castumerName', '=', record.name]), order='quantity', limit = '3')
+            record.bestSellerItems = bestSellerSearch
+    
+    
+
+
+    
